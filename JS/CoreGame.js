43af7dacef733675f3ee0962954d9a41aa4c1578
Kamzoki <kaittah@ymail.com>
2015-08-19 16:07:58 +0200

    :wq essage for your changes. Lines starting :wq ;wq :wq :q

--- JS/CoreGame.js
+++ JS/CoreGame.js
@@ -1,6 +1,24 @@
 // This is where Game loop runs
 
+//Drawing tools:-
 var layer1 , ctx1 , layer2 , ctx2 ;
+//menu , levels selecting , and entering buttons:-
+var menu , levels , intro;
+//Divs:-
+var start_Btn , htp_Btn , about_Btn , exit_Btn , lvl1_Btn;
+
+function  enterFun() {
+	// Enter button function
+	menu.style.display = "block";
+	intro.style.display = "none";
+}
+
+function startFun(){
+	//Start buton function
+	menu.style.display="none";
+	levels.style.display="block";
+}
+
 
 function init () {
 	// Initilaizing
@@ -8,4 +26,14 @@ function init () {
 	layer2 = document.getElementById('Layer2');
 	ctx1 = layer1.getContext('2d');
 	ctx2 = layer2.getContext('2d');
+	//-------------------------------------------
+	start_Btn = document.getElementById('Start');
+	htp_Btn = document.getElementById('Htp');
+	about_Btn = document.getElementById('About');
+	exit_Btn = document.getElementById('Exit');
+	lvl1_Btn = document.getElementById('Lvl1');
+	//------------------------------------------- 
+	menu = document.getElementById('Menu'); 
+	levels = document.getElementById('Levels'); 
+	intro = document.getElementById('Intro'); 
 }
