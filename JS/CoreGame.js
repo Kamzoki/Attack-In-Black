9a5ff9fb78c0585e1be6c87fdc778116d7aa3c85
Kamzoki <kaittah@ymail.com>
2015-08-19 15:19:40 +0200

    first committing

--- JS/CoreGame.js
+++ JS/CoreGame.js
@@ -1,53 +1,11 @@
-//Core Game 
+// This is where Game loop runs
 
-// main menu buttons Initializing	
-	var menu = document.getElementById('mainMenu');
-	var mbtn01 = document.getElementById('mBtn01');
-	var mbtn02 = document.getElementById('mBtn02');
-	var mbtn03 = document.getElementById('mBtn03');
-	var mbtn04 = document.getElementById('mBtn04');
-//----------------------------------------------------
-// level selecting buttons Initializing
-	var levelSel = document.getElementById('levelsMenu');
-	var lbtn01 = document.getElementById('lBtn01');
-	var lbtn02 = document.getElementById('lBtn02');
-	var lbtn03 = document.getElementById('lBtn03');
-//----------------------------------------------------
-// Intro elements Initializing
-	var introDiv = document.getElementById('Intro');
-	var ebtn = document.getElementById('enter');
-//----------------------------------------------------
-
+var layer1 , ctx1 , layer2 , ctx2 ;
 
 function init () {
-	// body...
-
-	// onloading function
-
-	var layer1 , ctx1 , layer2 , ctx2;
-	layer1 = document.getElementById('layer1');
-	layer2 = document.getElementById('layer2');
+	// Initilaizing
+	layer1 = document.getElementById('Layer1');
+	layer2 = document.getElementById('Layer2');
 	ctx1 = layer1.getContext('2d');
-	
-//----------------------------------------------------
-
-//----------------------------------------------------
-//LoadImages function call
-/*loadImages(source,function(images){
-
-	requestAnimationFrame();
-	});
-*/
+	ctx2 = layer2.getContext('2d');
 }
-
-function IntroDisplay(){
-	//Intro displaying none
-	introDiv.style.display="none";
-}
-
-function Level_Select (){
-	// levels selecting menu
-	levelSel.style.display = "block";
-	menu.style.display="none";
-}
-
