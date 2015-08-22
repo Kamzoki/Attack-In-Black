5c397f42e5544ade1c838e77b21d07a224a5c7fa
Kamzoki <kaittah@ymail.com>
2015-08-22 20:52:30 +0200

    level1 animation , done

--- JS/backGround.js
+++ JS/backGround.js
@@ -1,4 +1,4 @@
-//class background
+//class Ground
 //attributes
 /*{
 x,y,width,height,image,spdX
@@ -7,7 +7,7 @@ x,y,width,height,image,spdX
 /*{
 draw
 }*/
-function backGround(x,y,width,height,image)
+function backGround(x,y,width,height,image,SPDX)
 {
     //attributes
     this.x=x;
@@ -15,16 +15,19 @@ function backGround(x,y,width,height,image)
     this.width=width;
     this.height=height;
     this.image=image;
+    this.SPDX = SPDX;
     //functions
     this.draw=function(ctx)
     {
+        
         ctx.drawImage(this.image,this.x,this.y,this.width,this.height);
         ctx.drawImage(this.image,this.x+this.width,this.y,this.width,this.height);
-        this.x -= 1;
+        this.x -= this.SPDX;
         if (this.x < -this.width)
         {
             this.x = 0;
             ctx.clearRect(-this.width,this.y,this.width,this.height);
         }    
+
     }
 }
